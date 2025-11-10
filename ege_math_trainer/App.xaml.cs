50247b31984074dfc092d43ee153ecc18c4b8ca3
KolyaginaAV <KolyaginaAV@309D-4>
2025-11-10 09:39:31 +0700

    Добавьте файлы проекта.

---
+++ ege_math_trainer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ege_math_trainer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
