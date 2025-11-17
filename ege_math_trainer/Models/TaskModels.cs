b0ba21faaa96a936374e57aae98d1d91c4e439d6
KolyaginaAV@309D-4 <KolyaginaAV@309D-4>
2025-11-17 10:34:18 +0700

    Прописала работу кнопок входа/выхода/регистрации,  проверки, полкорректировала MainWindow

--- ege_math_trainer/Models/TaskModels.cs
+++ ege_math_trainer/Models/TaskModels.cs
@@ -3,11 +3,12 @@
    public abstract class Task 
     {
         public int Id { get; set; }
-        public string NameNumer { get; set; }
-        public string Condition { get; set; }
-        public string Decision { get; set; }
-        public string? ImageDecisionPath { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Condition { get; set; } //условие
         public string? ImageConditionPath { get; set; }
+        public string Decision { get; set; } //решение
+        public string? ImageDecisionPath { get; set; }
     }
     public class PartOneTask: Task
     {
@@ -16,5 +17,8 @@
     public class PartTwoTask: Task
     {
         public string EvaluationCriteria { get; set; }
+        public string ImageEvaluationCriteriaPath { get; set; }
+        public string IndEvaluationCriteria { get; set; }
+        public string ImageIndEvaluationCriteriaPath { get;set; }
     }
 }
