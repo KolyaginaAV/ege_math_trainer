a2a0e773f1006d5953dd44e77aeeb8b42e785abd
kolyagina <kolyagina_nastena@mail.ru>
2025-11-30 02:30:14 +0700

    bd

--- ege_math_trainer/Models/PartTwoTask.cs
+++ ege_math_trainer/Models/PartTwoTask.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Condition { get; set; } //условие
         public string? ConditionImage { get; set; }
-        public string Decision { get; set; } //решение
+        public string? Decision { get; set; } //решение
         public string? DecisionImagePath { get; set; }
         public string RightAnswer { get; set; }
         public string IndEvaluationCriteria { get; set; } //индивидуальные критерии для каждого задания
